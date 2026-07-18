00d193c9309071749466c51009f92937a993c5e9
nakagakigit <63888179+nakagakigit@users.noreply.github.com>
2026-07-19 07:50:20 +0900

    Create your_script_name.py

---
+++ your_script_name.py
@@ -0,0 +1,18 @@
+import os
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, PushMessageRequest, TextMessage
+
+# 環境変数から取得
+CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+USER_ID = os.environ.get('LINE_USER_ID')
+
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+
+with ApiClient(configuration) as api_client:
+    line_bot_api = MessagingApi(api_client)
+    line_bot_api.push_message(
+        PushMessageRequest(
+            to=USER_ID,
+            messages=[TextMessage(text='GitHub Actionsからの通知です')]
+        )
+    )
+    print("送信成功！")
