ad9b72183b506a2b30320ecf0647b8b66b40ddcd
nakagakigit <63888179+nakagakigit@users.noreply.github.com>
2026-07-20 12:46:22 +0900

    Update your_script_name.py

--- your_script_name.py
+++ your_script_name.py
@@ -3,18 +3,25 @@ from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, PushMes
 import yfinance as yf
 import pandas as pd
 
-# 環境変数から取得
-CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
-USER_ID = os.environ.get('LINE_USER_ID')
+def send_line_message(message_text):
+    """
+    LINEにメッセージを送信する関数
+    """
+    # 環境変数から取得
+    channel_access_token = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+    user_id = os.environ.get('LINE_USER_ID')
 
-configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+    configuration = Configuration(access_token=channel_access_token)
 
-with ApiClient(configuration) as api_client:
-    line_bot_api = MessagingApi(api_client)
-    line_bot_api.push_message(
-        PushMessageRequest(
-            to=USER_ID,
-            messages=[TextMessage(text='GitHub Actionsからの通知です')]
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.push_message(
+            PushMessageRequest(
+                to=user_id,
+                messages=[TextMessage(text=message_text)]
+            )
         )
-    )
-    print("送信成功！")
+        print("送信成功！")
+
+# 関数の呼び出し
+send_line_message('GitHub Actions関数の呼び出しからの通知です')
